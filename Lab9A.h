f7c7ee82f5d876d30a3c120f4cb410802b9d7590
Sindhuja Gudi <31775417+SindhujaGudi@users.noreply.github.com>
2017-09-11 12:24:30 -0500

    Add files via upload

---
+++ Lab9A.h
@@ -0,0 +1,25 @@
+//Lab9A.h
+//Name: Sindhuja Gudi
+//Section:01
+//Student ID:999992808
+
+#ifndef LAB9A_H_
+#define LAB9A_H_
+
+
+bool isOperator(const char c)
+{
+
+	if ((c == '+') || (c == '-') || (c == '*') || (c == '=') || (c == '/'))
+	{
+		return true;
+	}
+	return false;
+}
+
+#endif
+// Q7.Create a function that identifies the operator 
+   // Name of the function isOperator
+   // Formal parameter char
+   // return type bool 
+   // 0~9, a ~z, A ~ Z return true;
